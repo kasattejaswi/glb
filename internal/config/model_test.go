b0ceb7afa26e5fac9f8cff67e2a2f8fb9677b201
Tejaswi Kasat <kasattejasvi@gmail.com>
2023-05-15 18:09:58 +0530

    Added test cases for config and proxy

--- internal/config/model_test.go
+++ internal/config/model_test.go
@@ -1,11 +1,66 @@
 package config
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestReadConfig(t *testing.T) {
-	expectedConfig := Config{}
-	actualConfig := ReadConfig("testconfig.yaml")
-
+	t.Run("Should successfully read config file and store in struct", func(t *testing.T) {
+		expectedConfig := Config{
+			{
+				Path:      "/a",
+				Algorithm: "roundRobin",
+				Sticky:    false,
+				Hosts: []Hosts{
+					{
+						Protocol:              "http",
+						Hostname:              "localhost",
+						Port:                  8080,
+						MinHealthyHits:        5,
+						MinUnhealthyHits:      6,
+						HitFrequencyInSeconds: 20,
+						Health: Health{
+							Endpoint:    "/health",
+							SuccessCode: 200,
+							Method:      "GET",
+						},
+					},
+					{
+						Protocol:              "http",
+						Hostname:              "localhost",
+						Port:                  9090,
+						MinHealthyHits:        5,
+						MinUnhealthyHits:      6,
+						HitFrequencyInSeconds: 20,
+						Health: Health{
+							Endpoint:    "/health",
+							SuccessCode: 200,
+							Method:      "GET",
+						},
+					},
+				},
+			},
+		}
+		actualConfig := ReadConfig("model_test_res/testConfig.yaml")
+		if !reflect.DeepEqual(expectedConfig, actualConfig) {
+			t.Errorf("Expected and actual config didn't match. Expected: %v, Actual %v", expectedConfig, actualConfig)
+		}
+	})
+	t.Run("Should panic if file is not present", func(t *testing.T) {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Errorf("Expected function to panic")
+			}
+		}()
+		ReadConfig("nonexistingfile.yaml")
+	})
+	t.Run("Should panic if invalid config is provided", func(t *testing.T) {
+		defer func() {
+			if r := recover(); r == nil {
+				t.Errorf("Expected function to panic")
+			}
+		}()
+		ReadConfig("model_test_res/invalidConfig.yaml")
+	})
 }
--- internal/proxy/handler_test.go
+++ internal/proxy/handler_test.go
@@ -8,48 +8,77 @@ import (
 )
 
 func TestProxyToServerHandler(t *testing.T) {
-	// create a test server to use as the destination host
-	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("a", "b")
-		w.WriteHeader(201)
-		fmt.Fprint(w, "Hello, world!")
-	}))
-	defer testServer.Close()
+	t.Run("Should proxy requests successfully to the origin server", func(t *testing.T) {
+		// create a test server to use as the destination host
+		testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Add("a", "b")
+			w.WriteHeader(201)
+			fmt.Fprint(w, "Hello, world!")
+		}))
+		defer testServer.Close()
 
-	// create a request to pass to the ProxyToServerHandler function
-	req, err := http.NewRequest("GET", "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+		// create a request to pass to the ProxyToServerHandler function
+		req, err := http.NewRequest("GET", "/", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	// create a response recorder to capture the response from the ProxyToServerHandler function
-	rr := httptest.NewRecorder()
+		// create a response recorder to capture the response from the ProxyToServerHandler function
+		rr := httptest.NewRecorder()
 
-	// call the ProxyToServerHandler function with the test server as the destination host
-	handler := ProxyToServerHandler(testServer.URL)
-	handler.ServeHTTP(rr, req)
+		// call the ProxyToServerHandler function with the test server as the destination host
+		handler := ProxyToServerHandler(testServer.URL)
+		handler.ServeHTTP(rr, req)
 
-	// check that the response body is "Hello, world!"
-	expectedBody := "Hello, world!"
-	actualBody := rr.Body.String()
-	if actualBody != expectedBody {
-		t.Errorf("Unexpected response body. Expected: %s, Actual: %s", expectedBody, actualBody)
-	}
+		// check that the response body is "Hello, world!"
+		expectedBody := "Hello, world!"
+		actualBody := rr.Body.String()
+		if actualBody != expectedBody {
+			t.Errorf("Unexpected response body. Expected: %s, Actual: %s", expectedBody, actualBody)
+		}
 
-	// check that the response status code is 201 OK
-	expectedStatusCode := http.StatusCreated
-	actualStatusCode := rr.Code
-	if actualStatusCode != expectedStatusCode {
-		t.Errorf("Unexpected response status code. Expected: %d, Actual: %d", expectedStatusCode, actualStatusCode)
-	}
+		// check that the response status code is 201 OK
+		expectedStatusCode := http.StatusCreated
+		actualStatusCode := rr.Code
+		if actualStatusCode != expectedStatusCode {
+			t.Errorf("Unexpected response status code. Expected: %d, Actual: %d", expectedStatusCode, actualStatusCode)
+		}
 
-	// check if the response contains the headers returned by the server
-	expectedHeaderValue := "b"
-	actualHeaderValue := rr.Result().Header.Get("a")
-	if actualHeaderValue != expectedHeaderValue {
-		t.Errorf("Unexpeced header value. Expected %s, Actual %s", expectedHeaderValue, actualHeaderValue)
-	}
+		// check if the response contains the headers returned by the server
+		expectedHeaderValue := "b"
+		actualHeaderValue := rr.Result().Header.Get("a")
+		if actualHeaderValue != expectedHeaderValue {
+			t.Errorf("Unexpeced header value. Expected %s, Actual %s", expectedHeaderValue, actualHeaderValue)
+		}
 
-	// TODO: Add more test cases to cover other scenarios, such as errors returned by http.DefaultClient.Do()
+	})
+
+	t.Run("Should return 500 response if target server is down", func(t *testing.T) {
+
+		// create a test server to use as the destination host
+		testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			panic("")
+		}))
+		defer testServer.Close()
+
+		// create a request to pass to the ProxyToServerHandler function
+		req, err := http.NewRequest("GET", "/", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// create a response recorder to capture the response from the ProxyToServerHandler function
+		rr := httptest.NewRecorder()
+
+		// call the ProxyToServerHandler function with the test server as the destination host
+		handler := ProxyToServerHandler(testServer.URL)
+		handler.ServeHTTP(rr, req)
+		// check that the response status code is 201 OK
+		expectedStatusCode := http.StatusInternalServerError
+		actualStatusCode := rr.Code
+		if actualStatusCode != expectedStatusCode {
+			t.Errorf("Unexpected response status code. Expected: %d, Actual: %d", expectedStatusCode, actualStatusCode)
+		}
+	})
 
 }
